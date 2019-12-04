2de06457583d06f7eb209b782544999d814c7d9a
Michael Heumann <mheumann@comciencia.cl>
2019-12-04 19:48:06 -0300

    V. 2.2.30a

--- src/girocheckout-sdk/GiroCheckout_SDK_Config.php
+++ src/girocheckout-sdk/GiroCheckout_SDK_Config.php
@@ -55,7 +55,7 @@ class GiroCheckout_SDK_Config
    * @return string Version number of GiroCheckout SDK
    */
   static public function getVersion() {
-    return '2.2.31';
+    return '2.2.30a';
   }
 
   static public function getInstance() {
