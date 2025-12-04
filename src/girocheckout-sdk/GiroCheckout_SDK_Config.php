22c1de195d87c6747fd98fdb711b9c24fc0a5c62
girosolution <michael.heumann@s-publicservices.de>
2025-12-03 22:01:40 -0300

    billingAddress optional in ApplePay and GooglePay

--- src/girocheckout-sdk/GiroCheckout_SDK_Config.php
+++ src/girocheckout-sdk/GiroCheckout_SDK_Config.php
@@ -41,7 +41,7 @@ class GiroCheckout_SDK_Config
    * @return string Version number of GiroCheckout SDK
    */
   static public function getVersion() {
-    return '2.6.8.1';
+    return '2.6.8.2';
   }
 
   static public function getInstance() {
